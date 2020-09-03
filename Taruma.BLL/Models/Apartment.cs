e0d7a31d844f3d6df9324b8512ee3e8ff0e4cb33
Johnatan Lima <johw.23@outlook.com>
2020-09-02 23:31:18 -0300

    Implementação da classe Apartamento

--- Taruma.BLL/Models/Apartment.cs
+++ Taruma.BLL/Models/Apartment.cs
@@ -1,6 +1,29 @@
-﻿namespace Taruma.BLL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taruma.BLL.Models
 {
     public class Apartment
     {
+        public int ApartmentId { get; set; }
+
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        [Display(Name = "Number Apartment")]
+        [Range(1, 1000, ErrorMessage = "Invalid option")]
+        public int Number { get; set; }
+
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        [Range(1, 10, ErrorMessage = "Invalid option!")]
+        public int Floor { get; set; }
+
+        public string Image { get; set; }
+
+        public int ResidentId { get; set; }
+
+        public virtual User Resident { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public virtual User Owner { get; set; }
+
     }
 }
---
+++ Taruma.BLL/Models/Rental.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taruma.BLL.Models
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+
+
+        [Required(ErrorMessage = "Warning! Insert value here!")]
+        [Range(0, int.MaxValue, ErrorMessage = "Value invalid!")]
+        public decimal Value { get; set; }
+
+        [Display(Name = "Month")]
+        public int MonthId { get; set; }
+
+        public Month Month { get; set; }
+
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        [Range(2020, 2030, ErrorMessage = "Value invalid!")]
+        public int Year { get; set; }
+
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
