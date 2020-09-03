cf07baf49ca6f1c98e8e3423f779e5b3d3df8cf7
Johnatan Lima <johw.23@outlook.com>
2020-09-02 23:03:37 -0300

    Implementação de classe veículo

---
+++ Taruma.BLL/Models/Month.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taruma.BLL.Models
+{
+    public class Month
+    {
+        public int MonthId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Rental> Rentals { get; set; }
+
+        public virtual ICollection<RecourseHistoric> Historics { get; set; }
+    }
+}
--- Taruma.BLL/Models/Vehycle.cs
+++ Taruma.BLL/Models/Vehycle.cs
@@ -1,6 +1,31 @@
-﻿namespace Taruma.BLL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taruma.BLL.Models
 {
     public class Vehycle
     {
+        public int VehycleId { get; set; }
+
+        [Required(ErrorMessage = "Warning! Insert value here!")]
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Warning! Insert value here!")]
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        public string Brand { get; set; }
+
+
+        [Required(ErrorMessage = "Warning! Insert value here!")]
+        [StringLength(45, ErrorMessage = "Limit of caracter error!")]
+        public string Color { get; set; }
+
+
+        [Required(ErrorMessage = "Warning! Insert value here!")]
+        public string Plaque { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
     }
 }
