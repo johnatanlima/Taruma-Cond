a418741d715fc05221f67c91abcedf89c1b59521
Johnatan Lima <johw.23@outlook.com>
2020-09-05 22:56:16 -0300

    Definição de Interface e classe Repositório Genérico.

---
+++ Taruma.DAL/Interfaces/IRepositoryGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taruma.DAL.Interfaces
+{
+    public interface IRepositoryGeneric<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+        Task<TEntity> GetById(string id);
+        Task Insert(TEntity entity);
+
+        Task Insert(List<TEntity> entity);
+        Task Update(TEntity entity);
+        Task Delete(TEntity entity);
+        Task Delete(int id);
+        Task Delete(string id);
+    }
+}
---
+++ Taruma.DAL/Repositories/RepositoryGerenic.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Taruma.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taruma.DAL.Repositories
+{
+    public class RepositoryGeneric<TEntity> : IRepositoryGeneric<TEntity> where TEntity : class
+    {
+        private readonly Context _context;
+
+        public RepositoryGeneric(Context context)
+        {
+            _context = context;
+        }
+
+
+        public async Task Update(TEntity entity)
+        {
+            try
+            {
+                var update = _context.Set<TEntity>().Update(entity);
+                update.State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Delete(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                var entity = await GetById(id);
+                _context.Set<TEntity>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Delete(string id)
+        {
+            try
+            {
+                var entity = await GetById(id);
+                _context.Set<TEntity>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Insert(TEntity entity)
+        {
+            try
+            {
+                await _context.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Insert(List<TEntity> entity)
+        {
+            try
+            {
+                await _context.AddRangeAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            try
+            {
+                return await _context.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> GetById(string id)
+        {
+            try
+            {
+                return await _context.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            try
+            {
+                return await _context.Set<TEntity>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
