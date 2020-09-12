2c7dd6029b9319afe20a84dd04d78d8b33b280e1
Johnatan Lima <johw.23@outlook.com>
2020-09-12 16:24:17 -0300

    Desenvolvimento do método para registrar usuários.

--- Taruma/Controllers/UsersController.cs
+++ Taruma/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Taruma.BLL.Models;
 using Taruma.DAL.Interfaces;
@@ -13,12 +15,13 @@ namespace Taruma.Controllers
 {
     public class UsersController : Controller
     {
-        private readonly IUserRepository _userRepo;
+        private readonly IUserRepository _userRepository;
         private readonly IWebHostEnvironment _env;
 
-        public UsersController()
+        public UsersController(IUserRepository userRepository, IWebHostEnvironment env)
         {
-
+            _userRepository = userRepository;
+            _env = env;
         }
 
         public IActionResult Index()
@@ -35,6 +38,41 @@ namespace Taruma.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerVM, IFormFile photo)
         {
+
+            if (ModelState.IsValid)
+            {
+                if (photo != null)
+                {
+                    string myPath = Path.Combine(_env.ContentRootPath, "Photos");
+                    string photoName = Guid.NewGuid().ToString() + photo.FileName;
+
+                    using (FileStream fs = new FileStream(Path.Combine(myPath, photoName), FileMode.Create))
+                    {
+                        await photo.CopyToAsync(fs);
+
+                        registerVM.Photo = "~/Photos" + photoName;
+                    }
+                }
+
+                User user = new User();
+                IdentityUser userCreated;
+
+                if(_userRepository.HasRegister() == 0)
+                {
+                    user.UserName = registerVM.Name;
+                    user.CPF = registerVM.CPF;
+                    user.Email = registerVM.Email;
+                    user.PhoneNumber = registerVM.Phone;
+                    user.Photo = registerVM.Photo;
+                    user.Status = StatusAccount.Approved;
+                    user.FirstAccess = false;
+
+
+                }
+
+
+                return View(registerVM);
+            }
             
         }
     }
